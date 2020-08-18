e7b5d89515dc74a7ec05343267d0adc5c088b2e0
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-18 16:43:39 +0200

    global import

--- ProductivityTools.PSImportModulesFromDirectory/ProductivityTools.PSImportModulesFromDirectory.psm1
+++ ProductivityTools.PSImportModulesFromDirectory/ProductivityTools.PSImportModulesFromDirectory.psm1
@@ -5,16 +5,18 @@ function Import-ModulesFromDirectory {
 		[switch]$Recurse
 	)
 
+	Write-Verbose "Importing module from directory $Path" 
+	
 	$psm1s=Get-ChildItem -Path "$Path\*psm1" -Recurse:$Recurse
 	$module=$Path|split-path -leaf
-	Write-Verbose "Importing module from directory $Path" 
+	Write-Verbose "Found $(($psm1s).Length) psm1 files" 
+
 	foreach($psm1 in $psm1s)
 	{
 		Write-Host "Importing module from direcotry $($psm1.DirectoryName)" -ForegroundColor DarkGreen
 		$leaf=$psm1.FullName|split-path -leaf
 		Write-Host "-File $leaf" -ForegroundColor Gray
-		Import-Module $psm1.FullName
-		
+		Import-Module $psm1.FullName -Global
 	}
 }
 Export-ModuleMember Import-ModulesFromDirectory 
